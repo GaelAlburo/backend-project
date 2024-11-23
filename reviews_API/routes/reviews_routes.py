2daaa45c95dba466650d0470c30debbbde0175f8
GaelAlburo <gael.a24@outlook.com>
2024-11-23 01:40:48 -0600

    feat(reviews-route): add /api/v1/reviews/<int:review_id> PUT route

--- reviews_API/routes/reviews_routes.py
+++ reviews_API/routes/reviews_routes.py
@@ -14,6 +14,9 @@ class ReviewRoute(Blueprint):
     def register_routes(self):
         self.route("/api/v1/reviews", methods=["GET"])(self.get_reviews)
         self.route("/api/v1/reviews", methods=["POST"])(self.add_review)
+        self.route("/api/v1/reviews/<int:review_id>", methods=["PUT"])(
+            self.update_review
+        )
 
     def get_reviews(self):
         reviews = self.review_service.get_all_reviews()
@@ -62,3 +65,33 @@ class ReviewRoute(Blueprint):
         except Exception as e:
             self.logger.error(f"Error adding review: {e}")
             return jsonify({"error": f"Error adding review: {e}"}), 500
+
+    def update_review(self, review_id):
+        try:
+            user, product, review, rating = self.fetch_request_data()
+
+            try:
+                self.review_schema.validates_user(user)
+                self.review_schema.validates_product(product)
+                self.review_schema.validates_review(review)
+                self.review_schema.validates_rating(rating)
+            except ValidationError as e:
+                return jsonify({"error": f"Invalid data: {e}"}), 400
+
+            update_review = {
+                "_id": review_id,
+                "user": user,
+                "product": product,
+                "review": review,
+                "rating": rating,
+            }
+
+            updated_review = self.review_service.update_review(review_id, update_review)
+            if updated_review:
+                return jsonify(updated_review), 200
+            else:
+                return jsonify({"error": "Review not found"}), 404
+
+        except Exception as e:
+            self.logger.error(f"Error updating review: {e}")
+            return jsonify({"error": f"Error updating review: {e}"}), 500
