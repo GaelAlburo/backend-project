25e63a065a1d97cd3a8ad9d0ebcc560fb879cdd1
GaelAlburo <gael.a24@outlook.com>
2024-11-23 15:39:21 -0600

    feat(reviews): add app.py file to test reviews_API routes

---
+++ reviews_API/app.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from reviews_API.models.reviews_model import ReviewModel
+from reviews_API.services.reviews_services import ReviewService
+from reviews_API.schemas.reviews_schemas import ReviewSchema
+from reviews_API.routes.reviews_routes import ReviewRoute
+from flask_cors import CORS
+from flasgger import Swagger
+
+app = Flask(__name__)
+CORS(app)
+
+# Swagger
+swagger = Swagger(app)
+
+# Model
+db_conn = ReviewModel()
+db_conn.connect_to_database()
+
+# Service
+review_service = ReviewService(db_conn)
+
+# Schema
+review_schema = ReviewSchema()
+
+# Routes
+review_routes = ReviewRoute(review_service, review_schema)
+
+app.register_blueprint(review_routes)
+
+if __name__ == "__main__":
+    try:
+        app.run(debug=True)
+    finally:
+        db_conn.close_connection()
